d246fbf865cfecaf2b89d91813cb6aae89b25b7d
Lilian Nguyen <92119959+lil-nguyen@users.noreply.github.com>
2022-01-18 14:19:42 -0600

    Add behavior to display restaurants

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,11 +1,17 @@
 import {useEffect} from 'react';
 
-export const RestaurantList = ({loadRestaurants}) => {
+export const RestaurantList = ({loadRestaurants, restaurants}) => {
   useEffect(() => {
     loadRestaurants();
   }, [loadRestaurants]);
 
-  return <div>RestaurantList</div>;
+  return (
+    <div>
+      {restaurants.map(restaurant => (
+        <li key={restaurant.id}>{restaurant.name}</li>
+      ))}
+    </div>
+  );
 };
 
 export default RestaurantList;
--- src/components/__tests__/RestaurantList.spec.js
+++ src/components/__tests__/RestaurantList.spec.js
@@ -1,12 +1,34 @@
 import {render} from '@testing-library/react';
 import {RestaurantList} from '../RestaurantList';
 
+// It's important to check only one behavior per test: it makes it easy to understand what it does, and what actually goes wrong.
+
 describe('RestaurantList', () => {
   it('loads restaurants on first render', () => {
     const loadRestaurants = jest.fn().mockName('loadRestaurants');
-
-    render(<RestaurantList loadRestaurants={loadRestaurants} />);
+    const restaurants = [];
+    render(
+      <RestaurantList
+        loadRestaurants={loadRestaurants}
+        restaurants={restaurants}
+      />,
+    );
 
     expect(loadRestaurants).toHaveBeenCalled();
+  });
+
+  it('displays the restaurants', () => {
+    const noop = () => {};
+    const restaurants = [
+      {id: 1, name: 'Sushi Place'},
+      {id: 2, name: 'Pizza Place'},
+    ];
+
+    const {queryByText} = render(
+      <RestaurantList loadRestaurants={noop} restaurants={restaurants} />,
+    );
+
+    expect(queryByText('Sushi Place')).not.toBeNull();
+    expect(queryByText('Pizza Place')).not.toBeNull();
   });
 });
